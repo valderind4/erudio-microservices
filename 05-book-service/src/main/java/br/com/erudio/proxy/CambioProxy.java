be30b6d39febdba4d8b19f855f78682d3abed155
valderi <valderimedeiros@gmail.com>
2022-08-04 10:24:14 -0300

    Final Implementation

---
+++ 05-book-service/src/main/java/br/com/erudio/proxy/CambioProxy.java
@@ -0,0 +1,17 @@
+package br.com.erudio.proxy;
+
+import br.com.erudio.response.Cambio;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "cambio-service", url = "localhost:8000")
+public interface CambioProxy {
+
+    @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
+    public Cambio getCambio(
+            @PathVariable("amount") Double amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    );
+}
---
+++ 05-book-service/src/main/java/br/com/erudio/response/Cambio.java
@@ -0,0 +1,23 @@
+package br.com.erudio.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cambio implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String from;
+    private String to;
+    private Double conversionFactor;
+    private Double convertedValue;
+    private String environment;
+}
